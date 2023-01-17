4d00079b05ea1f39749f67c8fa98dbfad2a39f44
八憲 黑 <keson96351@gmail.com>
2023-01-17 16:30:17 +0800

    finish key word scraping

--- web_for_sever/web_api/api_for_web.py
+++ web_for_sever/web_api/api_for_web.py
@@ -1,77 +1,46 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 from typing import Optional
+
+from fastapi import FastAPI,Query
 from pydantic import BaseModel
+
+from web_for_sever.web_api.scraping_schema import DayResponseListModel
 from web_for_sever.web_scraping.key_words_scraping import hot_key_dict
 
-def html_create():
-    html_contents ="""
-<head>
-    <title>LifeTool</title>
-</head>
-<body>
-    <div class="above_selection">
-        <a id="web_Home" href="guest.html">LifeTool</a>
-        <a href="date.html">Date</a>
-        <a>Weather</a>
-        <a>Note</a>
-        <a>Flavorite Web</a>
-        <a>Keys</a>
-        <a>Login</a>
-    </div>
-    <div>
-        <p>Home</p>
-    </div>
-</body>
-    """
-    return HTMLResponse(content=html_contents,status_code=200)
-
+#create API , name is app.
 title = "My API"
-
 app=FastAPI(title=title)
 
+# API "/" using GET() return {}
 @app.get(
     "/",
-    tags=["Guest"]
+    tags=["Root"]
 )
-def welcome(page : Optional[str] = None):
-    if page:
-        return f"There is Page {page} !"
-    else:
-        return "There is no Page !"
+def get_homePage():
+    return {}
 
-
+geo_list=[
+    
+]
 @app.get(
-    "/ok",
-    tags=["Guest"]
-)
-def sayhello():
-    return "Hello"
-
-class Charact(BaseModel):
-    name : str
-    age : Optional[int]
-    isMale : bool
-
-@app.post(
-    "/charact",
-    tags=["Player"]
-)
-def create_charact(player : Charact):
-    return f"Your Charact is created with name as {player.name}"
-
-
-@app.get(
-    "/guest.html"
-)
-def see_web():
-    return html_create()
-
-@app.post(
     "/hot_key",
-    tags=["web"]
+    tags=["Hot Key"],
+    response_model=DayResponseListModel
 )
-def search_key( area : Optional[str] = None ):
-    if area :
-        return hot_key_dict(area)
-    return hot_key_dict()
+async def search_key( 
+    geo : Optional[str] = Query(None , description="The country's name that you are searching for(using two charact)."),
+    endDate : Optional[str] = Query("" , description="The end you want search !")
+): 
+    if endDate != "":
+        endDate = f"&ed={endDate}"
+    if geo ==None:
+        geo = "tw"
+    
+    return await hot_key_dict(country=geo,endDate=endDate)
+
+@app.get(
+    "/time",
+    tags=["Date & Time"],
+    response_model=None
+)
+def get_time():
+    pass
--- web_for_sever/web_scraping/key_words_scraping.py
+++ web_for_sever/web_scraping/key_words_scraping.py
@@ -1,57 +1,68 @@
-import requests
 import json
 
+import requests
 
-def hot_key_dict(country : str ="TW") -> dict :
+from web_for_sever.web_api.scraping_schema import (KeyResponseModel, DayResponseModel, ImageResponseModel, DayResponseListModel)
+
+async def get_from_link(link):
+    info = requests.get(link)
+    data = info.text
+    info.close()
+    return data
+
+async def hot_key_dict(country : str ="TW" , endDate : str = "") -> dict :
+    
     country = country.upper()
-    url = "https://trends.google.com.tw/trends/api/dailytrends?hl=zh-TW&tz=-480&geo={}&ns=15"
-    url = url.format(country)
+    url = "https://trends.google.com.tw/trends/api/dailytrends?hl=zh-TW&tz=-480{}&geo={}&ns=15"
+    url = url.format(endDate,country)
 
-    data =None
+    datas_text = await get_from_link(url)
+    datas_text = datas_text[5:]
+    datas_json = json.loads(datas_text)
+    del datas_text
 
-    with requests.get(url) as req:
-        data = req.text
+    hot_key_info = DayResponseListModel()
+    hot_key_info.endDateForNextRequest = datas_json["default"]["endDateForNextRequest"]
+    days_key_info = []
+    
+    days_datas = datas_json["default"]["trendingSearchesDays"];del datas_json
+    for day_datas in days_datas:
+        
+        keys_of_day = DayResponseModel()
+        keys_of_day.day_date_int = int(day_datas["date"])
+        keys_of_day.day_date_string = day_datas["formattedDate"]
 
-    data = data[5:]
-    data = json.loads(data)
+        keys_datas = day_datas["trendingSearches"]
 
-    data = data["default"]["trendingSearchesDays"][0]
-    data_date = data["date"]
-    data_date_string = data["formattedDate"]
+        keys_list = []
 
-    data = data["trendingSearches"]
+        keys_amoung = len(keys_datas)
+        for i in range(keys_amoung):
 
-    data_amoung = len(data)
-    if data_amoung>10:
-        data_amoung=10
+            key_data = keys_datas[i]
 
-    data_list = []
+            key = KeyResponseModel()
+            key.key_title = key_data["title"]["query"]
+            key.key_hot = key_data["formattedTraffic"]
 
-    for i in range(data_amoung):
-        unit_data_dict = {}
+            image = ImageResponseModel()
+            try:
+                image.image_link = key_data["image"]["imageUrl"]
+            except:
+                del image
+            else:
+                image.source = key_data["image"]["source"]
+                image.news_link = key_data["image"]["newsUrl"]
+                key.image = image
 
-        one_data = data[i]
+            keys_list.append(key)
+        keys_of_day.key_list = keys_list
+        
+        days_key_info.append(keys_of_day)
+    
+    hot_key_info.day_list=days_key_info
 
-        unit_data_dict["formattedTraffic"] = one_data["formattedTraffic"]
-        unit_data_dict["title"] = one_data["title"]["query"]
-
-        unit_data_img = {}
-
-        unit_data_img["imageUrl"] = one_data["image"]["imageUrl"]
-        unit_data_img["newsUrl"] = one_data["image"]["newsUrl"]
-        unit_data_img["source"] = one_data["image"]["source"]
-
-        unit_data_dict["image"] = unit_data_img
-
-        data_list.append(unit_data_dict)
-
-    response_dict = {
-        "data_date" : data_date,
-        "data_date_string" : data_date_string,
-        "day_hot_key" : data_list
-    }
-
-    return response_dict
+    return hot_key_info
 
 # web=hot_key_dict("GB")
 # with open("data.json","w") as file:
